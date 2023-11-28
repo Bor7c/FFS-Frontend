742fc33762e32fe676e96ea15bcc384b894259c0
Bor7c <Bob.sokolov@yandex.ru>
2023-11-28 19:58:31 +0300

    new

---
+++ FFS/src/modules/GetBreaches.ts
@@ -0,0 +1,9 @@
+export interface Breaches {
+    fine_id: number;
+    picture_url: string;
+    title: string;
+    text: string;
+    price: string;
+    fine_status: string;
+    image: string;
+}
