d591d674b865d8497bba74d321ecad87b3999aac
Bor7c <Bob.sokolov@yandex.ru>
2023-12-12 20:21:42 +0300

    new

--- FFS/App.js
+++ FFS/App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import ShopScreen from './pages/FinesPage/FinesPage';
+import DeviceScreen from './pages/FinePage/FinePage';
+// import { store } from './store';
+// import { Provider } from 'react-redux';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>ХУЙ</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        // <Provider store={store}>
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen name='Fines' component={ShopScreen} />
+                    <Stack.Screen name='Fine' component={DeviceScreen} />
+                </Stack.Navigator>
+            </NavigationContainer>
+        // </Provider>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
